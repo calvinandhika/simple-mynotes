ea66bea319f7730659353525123843859825b1b8
calvinandhika <calvin.andhika@gmail.com>
2022-11-02 08:50:32 +0700

    step-2a

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc_vandad/firebase_options.dart';
 import 'package:flutter_bloc_vandad/views/login_view.dart';
 import 'package:flutter_bloc_vandad/views/register_view.dart';
+import 'package:flutter_bloc_vandad/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,14 +31,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            // return const Text('Done');
-
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
 
           default:
             return const CircularProgressIndicator();
